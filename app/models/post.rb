5e17c9f8ba1797e8698e72827139b0e8bf09d91c
maritora <yp.318myu@gmail.com>
2022-07-26 04:36:34 +0900

    ばりでーしょんの修正を行いました

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,18 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :stday, presence: true
   validates :endday, presence: true
-  validates :shujitsu, presence: true
-  validates :memo, presence: true
+  validate :date_before_start
+  validate :date_before_finish
+
+  def date_before_start
+    unless stday == nil
+    errors.add(:stday, "は今日以降のものを選択してください") if stday < Date.today
+    end
+  end
+
+  def date_before_finish
+    return if endday.blank? || stday.blank?
+    errors.add(:endday, "は開始日以降のものを選択してください") if endday < stday
+  end
+
 end
